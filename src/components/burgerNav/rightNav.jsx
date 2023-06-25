9dab7bfc5ccf901762eff419af9cfbec840a2ac4
KayT98 <khanhtruong109@yahoo.com>
2023-06-24 23:24:27 -0500

    hamburger navbar

---
+++ src/components/burgerNav/rightNav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RightNav = () => {
+    return (
+        <ul>
+            <li>About me</li>
+            <li>Skills</li>
+            <li>Coding Projects</li>
+            <li>Photoshop Projects</li>
+            <li>Contact me!</li>
+        </ul>
+    )
+} 
+
+export default RightNav
